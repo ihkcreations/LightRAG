51c23802ea2ca5909de5779f4da5d15887c2287a
ihkcreations <laramin.astra@gmail.com>
2025-03-11 12:53:55 +0600

    update qwen2.5 14b added

--- examples/lightrag_ollama_demo.py
+++ examples/lightrag_ollama_demo.py
@@ -23,7 +23,7 @@ async def initialize_rag():
     rag = LightRAG(
         working_dir=WORKING_DIR,
         llm_model_func=ollama_model_complete,
-        llm_model_name="deepseek-r1:8b",
+        llm_model_name="qwen2.5:14b",
         llm_model_max_async=4,
         llm_model_max_token_size=32768,
         llm_model_kwargs={
